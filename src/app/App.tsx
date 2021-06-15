588f0b542a5dc54621e027b5ed18f8660fb06828
Shubham Mathur <shumathu@redhat.com>
2021-06-15 09:47:38 +0530

    add color

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import "./App.scss";
 import "@patternfly/react-core/dist/styles/base.css";
 import AppLayout from "./components/main-components/AppLayout/AppLayout";
 import SummaryCard from "./components/main-components/Summary/Summary";
-import OverviewCard from "./components/main-components/Overview/overview";
+import OverviewCard from "./components/main-components/Overview/Overview";
 import Table from "./components/main-components/Table/Table";
 import GlobalStateProvider from "./store/GlobalStateProvider";
 import { GetStackDetails } from "./utils/apiCalls";
